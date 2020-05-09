0731bc1bae2878ac62b30fbf4358a332aa67930c
varadlanke <lanke.varad@gmail.com>
2020-05-09 19:37:12 +0530

    <Add> AddressBookManager Class with methods and fields

---
+++ AddressBookManager.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class AddressBookManager{
+	List<AddressBook> addressListBooks = new ArrayList<AddressBook>();
+	Scanner userScanner = new Scanner(System.in);
+
+	public void newAddressBook(){}
+	public void openAddressBook(){}
+	public void closeAddressBook(){}
+	public void saveAddressBook(){}
+}
